cae26d0ac05604cb3b14236cea9a8b6f3e3130de
ah01 <horcicaa@gmail.com>
2018-11-04 11:26:30 +0100

    Install tmux

--- ah-linux-setup.sh
+++ ah-linux-setup.sh
@@ -8,6 +8,8 @@ function replace-file
 
 # TMux
 
+sudo apt-get install tmux
+
 replace-file ~/.bash_profile https://raw.githubusercontent.com/ah01/linux-setup/master/.bash_profile
 replace-file ~/.tmux.conf https://raw.githubusercontent.com/ah01/linux-setup/master/.tmux.conf
 
