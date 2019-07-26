44b92a4f888030e0a99f12b07974feac62378a4d
Adam Hořčica <horcicaa@gmail.com>
2019-07-26 08:49:06 +0200

    Update .welcome-info.sh

--- user/.welcome-info.sh
+++ user/.welcome-info.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-IPs=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1' | grep -v '172\.'`
 HOSTN=`hostname`
 UP=`uptime -p`
+
+if hash ip 2>/dev/null; then
+    IPs=`ip address show | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+else
+    IPs=`ifconfig | grep -Eo 'inet (addr:)?([0-9]*\.){3}[0-9]*' | grep -Eo '([0-9]*\.){3}[0-9]*' | grep -v '127.0.0.1'`
+fi
 
 echo -e "\n\033[0;34m$HOSTN\033[0m (${IPs//$'\n'/ }) \033[0;32m$UP\033[0m \n"
 
